ea3db04092a209caaed553c9cb46fc5b4af2ba31
Abhisekh Nag <abhisekhnag2504@gmail.com>
2021-12-06 22:04:14 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./App.css";
+
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="card">
+        <div className="header">
+          <div className="logo">
+            <a href=".">PICE</a>
+          </div>
+        </div>
+        <div className="content">
+          <div className="title-holder">
+            <h1>Coming Soon</h1>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
